446b842c239eb9dd1b4912ee5636e506c5a1bfe4
Derek Troy-West <derek@troywest.com>
2017-08-11 16:32:24 +1000

    break out ns

--- arche/test/troy_west/arche_test.clj
+++ arche/test/troy_west/arche_test.clj
@@ -1,10 +1,2 @@
 (ns troy-west.arche-test
-  (:require [clojure.test :refer [deftest is]]
-            [qbits.alia :as alia])
-  (:import com.datastax.driver.core.SimpleStatement
-           troy_west.arche.IBindable))
-
-(deftest query->statement-test
-  (let [statement (alia/query->statement ::test {} nil)]
-    (is (instance? SimpleStatement statement))
-    (is (instance? troy_west.arche.IBindable statement))))
+  (:require [clojure.test :refer [deftest is]]))
