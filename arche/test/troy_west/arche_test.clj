f318ad38e04507ffa339750676154a67d7a2d8e5
Tristan Straub <tristan@troywest.com>
2017-08-11 15:22:33 +1000

    Fix IBindable import

--- arche/test/troy_west/arche_test.clj
+++ arche/test/troy_west/arche_test.clj
@@ -1,11 +1,10 @@
 (ns troy-west.arche-test
-  (:require [clojure.test :refer [deftest is use-fixtures]]
-            [troy-west.arche :as arche]
+  (:require [clojure.test :refer [deftest is]]
             [qbits.alia :as alia])
-  (:import [com.datastax.driver.core SimpleStatement]))
+  (:import com.datastax.driver.core.SimpleStatement
+           troy_west.arche.IBindable))
 
 (deftest query->statement-test
   (let [statement (alia/query->statement ::test {} nil)]
     (is (instance? SimpleStatement statement))
     (is (instance? troy_west.arche.IBindable statement))))
-
