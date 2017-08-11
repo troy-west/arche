46e985c109d5ffe40103c905e66cb80037a45784
tristanstraub <tristan@straub.id.au>
2017-08-11 16:27:34 +1000

    Circleci (#11)
    * CircleCi yaml with lein modules install, and deps
    * Fix IBindable import
    * Test selectors for integration tests

--- arche/project.clj
+++ arche/project.clj
@@ -6,6 +6,8 @@
 
   :plugins [[lein-modules "0.3.11"]]
 
+  :test-selectors {:default (complement :integration)}
+
   :profiles {:dev {:resource-paths ["test-resources"]
                    :dependencies [[com.smxemail/ccm-clj "1.1.0"]
                                   [ch.qos.logback/logback-classic "1.1.8"]]}})
