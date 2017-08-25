f6f6cf1471c42d0b9f05c87f78148d98e5fd1528
Derek Troy-West <derek@troywest.com>
2017-08-25 14:18:21 +1000

    all tests working

--- test/troy_west/arche/integration_test.clj
+++ test/troy_west/arche/integration_test.clj
@@ -2,8 +2,7 @@
   (:require [clojure.test :refer [deftest is use-fixtures]]
             [troy-west.arche :as arche]
             [troy-west.arche.integration-fixture :as fixture]
-            [ccm-clj.core :as ccm]
-            [qbits.alia :as alia])
+            [ccm-clj.core :as ccm])
   (:import [com.datastax.driver.core UDTValue Session]))
 
 (use-fixtures :once (fn [test-fn]
@@ -11,18 +10,22 @@
                       (test-fn)
                       (fixture/stop-system!)))
 
-(deftest connection-wrapper-test
-  (is (instance? Session (:connection @fixture/system))))
-
 (deftest encoders-test
+
   (let [encoded (arche/encode-udt (:connection @fixture/system)
-                                  ::asset {:code     "AB"
-                                           :currency "GBP"
-                                           :notional "12"})]
+                                  :arche/asset {:code     "AB"
+                                                :currency "GBP"
+                                                :notional "12"})]
     (is (instance? UDTValue encoded))))
 
-(deftest statement-query-test
-  (let [session (:connection @fixture/system)]
-    (alia/execute session ::insert-client {:values {:id "id-1" :name "Carol"}})
+(deftest insert-select-test
+
+  (let [connection (fixture/connection)]
+
     (is (= {:id "id-1", :name "Carol"}
-           (first (alia/execute session ::select-client {:values {:id "id-1"}}))))))
+           (do (arche/execute connection
+                              :arche/insert-client
+                              {:values {:id "id-1" :name "Carol"}})
+               (first (arche/execute connection
+                                     :arche/select-client
+                                     {:values {:id "id-1"}})))))))
