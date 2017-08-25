bacbe55a066c0110c15cc023f6d37ec435ecd324
Derek Troy-West <derek@troywest.com>
2017-08-25 17:11:33 +1000

    dual mode tests

--- arche/project.clj
+++ arche/project.clj
@@ -4,10 +4,7 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :plugins [[lein-modules "0.3.11"]]
-
   :dependencies [[clojure-future-spec "_"]]
 
-  :profiles {:dev {:resource-paths ["test-resources"]
-                   :dependencies [[com.smxemail/ccm-clj "1.1.0"]
-                                  [ch.qos.logback/logback-classic "1.1.8"]]}})
+  :plugins [[lein-modules "0.3.11"]])
+
