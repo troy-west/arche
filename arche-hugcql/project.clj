c4e51f8ea6d3a4d13d0ea96d7c7c71e09328e033
Derek Troy-West <derek@troywest.com>
2018-02-13 17:16:12 +1100

    Update project.clj

--- arche-hugcql/project.clj
+++ arche-hugcql/project.clj
@@ -1,6 +1,8 @@
 (defproject com.troy-west/arche-hugcql "0.3.1-SNAPSHOT"
   :description "Arche: HugCQL module"
 
+  :url "https://github.com/troy-west/arche"
+  
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
