86524a0cb9fab01c1f81b8e9cfc6650731e08249
Derek Troy-West <derek@troywest.com>
2018-02-13 17:17:24 +1100

    Update project.clj

--- arche/project.clj
+++ arche/project.clj
@@ -1,7 +1,7 @@
 (defproject com.troy-west/arche "0.3.1-SNAPSHOT"
   :description "Arche: A Clojure Battery Pack for Alia / Cassandra"
 
-  :url "http://www.troy-west.com/arche"
+  :url "https://github.com/troy-west/arche"
 
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
