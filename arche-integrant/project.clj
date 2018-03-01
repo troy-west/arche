c244519a67b0cb88e452ca6c70c0ddc70fe3a9fe
Derek Troy-West <derek@troywest.com>
2018-03-01 15:40:47 +1100

    Version 0.3.3

--- arche-integrant/project.clj
+++ arche-integrant/project.clj
@@ -1,4 +1,4 @@
-(defproject com.troy-west/arche-integrant "0.3.3-SNAPSHOT"
+(defproject com.troy-west/arche-integrant "0.3.3"
   :description "Arche: Integrant module"
 
   :url "https://github.com/troy-west/arche"
