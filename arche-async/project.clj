ae7e725ba0037342e3ab3b52b81ed8242709ac8e
Derek Troy-West <derek@troywest.com>
2018-04-17 12:47:38 +1000

    revert explicit core.async dependency management

--- arche-async/project.clj
+++ arche-async/project.clj
@@ -2,12 +2,11 @@
   :description "Arche: Alia / core.async module"
 
   :url "https://github.com/troy-west/arche"
-  
+
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
   :plugins [[lein-modules "0.3.11"]]
 
-  :dependencies [[org.clojure/core.async "0.4.474"]
-                 [com.troy-west/arche "_"]
-                 [cc.qbits/alia-async "_" :exclusions [org.clojure/core.async]]])
+  :dependencies [[com.troy-west/arche "_"]
+                 [cc.qbits/alia-async "_"]])
